8a4926cc05202a5d694d1d53cf577ac41c9b26d0
quenox <56411642+quenox@users.noreply.github.com>
2025-04-25 02:56:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -144,4 +144,33 @@ document.addEventListener('DOMContentLoaded', () => {
      //    });
      // }
 
+
+
+    /* para el modal */
+    // Referencias
+    const modal = document.getElementById("image-modal");
+    const modalImg = document.getElementById("modal-img");
+    const specialPaddlePhoto = document.getElementById("special-paddle-photo");
+    const closeBtn = document.querySelector(".close");
+    
+    // Abrir modal al hacer clic en la imagen
+    specialPaddlePhoto.addEventListener("click", () => {
+        modal.style.display = "block";
+        modalImg.src = specialPaddlePhoto.src;
+    });
+    
+    // Cerrar modal al hacer clic en la "X"
+    closeBtn.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+    
+    // Cerrar modal al hacer clic fuera de la imagen
+    modal.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+
+
+    
 }); // Fin de DOMContentLoaded
